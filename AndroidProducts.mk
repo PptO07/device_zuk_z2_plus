65f0dd78e95c94ee4693db32c1b09b577f5214c5
Pranav Temkar <pranavtemkar@gmail.com>
2022-09-18 17:45:53 +0000

    z2_plus: init Xtended
     - Xtended Bringup
     - Official Build
     - Define Maintainer Name, Processor model & Enable Blur
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-$(LOCAL_DIR)/voltage_z2_plus.mk
+$(LOCAL_DIR)/xtended_z2_plus.mk
--- xtended_z2_plus.mk
+++ xtended_z2_plus.mk
@@ -20,10 +20,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from z2_plus device
 $(call inherit-product, device/zuk/z2_plus/device.mk)
 
-# Inherit some common VoltageOS stuff.
-$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+# Inherit some common Project Xtended stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
-PRODUCT_NAME := voltage_z2_plus
+# Official MSM-Xtended
+XTENDED_BUILD_TYPE := OFFICIAL
+
+# Maintainer
+XTENDED_BUILD_MAINTAINER := 🔥PranavTemkar🔥
+
+# SOC Model
+PROCESSOR_MODEL := MSM8996
+
+# Blur
+TARGET_ENABLE_BLUR := true
+
+PRODUCT_NAME := xtended_z2_plus
 PRODUCT_DEVICE := z2_plus
 PRODUCT_MANUFACTURER := ZUK
 PRODUCT_BRAND := ZUK
@@ -37,11 +49,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=z2_plus PRODUCT_NAME=z2_plus
 
 TARGET_VENDOR := zuk
 
-VOLTAGE_BUILD_TYPE = OFFICIAL
-
-TARGET_BUILD_GRAPHENEOS_CAMERA = true
-
-TARGET_BOOT_ANIMATION_RES := 1280-48
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
